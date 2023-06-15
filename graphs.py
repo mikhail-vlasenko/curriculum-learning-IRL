6c0153252a750b792408449fc315a9c7733351c8
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2023-06-15 14:34:10 +0200

    beautify the graph plotting tool

--- graphs.py
+++ graphs.py
@@ -5,41 +5,74 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv('graph_data/fixed_airl.csv')
-old_runs = [121, 122, 123]
-new_runs = [162, 163, 166]
-columns = []
-for c in df.columns:
-    if "__MIN" not in c and "__MAX" not in c:
-        columns.append(c)
-df = df[columns]
-df.fillna(method='ffill', inplace=True)
+def clean_df(df):
+    columns = []
+    for c in df.columns:
+        if "__MIN" not in c and "__MAX" not in c:
+            columns.append(c)
+    df = df[columns]
+    df = df.fillna(method='ffill')
+    return df
 
-old_columns = []
-new_columns = []
-for c in df.columns:
-    for o in old_runs:
-        if str(o) in c:
-            old_columns.append(c)
-    for n in new_runs:
-        if str(n) in c:
-            new_columns.append(c)
 
-df['old_mean'] = df[old_columns].mean(axis=1)
-df['new_mean'] = df[new_columns].mean(axis=1)
-df['old_std'] = df[old_columns].std(axis=1)
-df['new_std'] = df[new_columns].std(axis=1)
+def process_and_plot(
+        df, run_groups, group_names,
+        smoothing_window=5, title='True reward in the environment'
+):
+    processed_columns = []
+    means = []
+    stds = []
 
-fig, ax = plt.subplots()
+    # Process each group
+    for i, group in enumerate(run_groups):
+        group_columns = []
 
-ax.plot(df['Step'], df['old_mean'], label='old')
-ax.plot(df['Step'], df['new_mean'], label='new')
-ax.fill_between(df['Step'], df['old_mean'] - df['old_std'], df['old_mean'] + df['old_std'], alpha=0.4)
-ax.fill_between(df['Step'], df['new_mean'] - df['new_std'], df['new_mean'] + df['new_std'], alpha=0.4)
-# ax.fill_between(xs, lower, upper, alpha=0.4)
+        # Filter group columns
+        for c in df.columns:
+            for run_id in group:
+                if str(run_id) in c:
+                    group_columns.append(c)
 
-# ax.set_xlabel('$n$')
-# ax.set_ylabel('$m$ [%]')
-ax.legend()
-ax.set_title('True reward in the environment')
-plt.show()
+        # Smooth group columns
+        for c in group_columns:
+            df[c] = df[c].rolling(smoothing_window).mean()
+
+        # Compute mean and standard deviation
+        mean_col = f'group_{i}_mean'
+        std_col = f'group_{i}_std'
+        df[mean_col] = df[group_columns].mean(axis=1)
+        df[std_col] = df[group_columns].std(axis=1)
+
+        # Append processed columns and names
+        processed_columns.append(group_columns)
+        means.append(mean_col)
+        stds.append(std_col)
+
+    # Plot
+    fig, ax = plt.subplots()
+    for mean, std, name in zip(means, stds, group_names):
+        ax.plot(df['Step'], df[mean], label=f'{name}')
+        ax.fill_between(df['Step'], df[mean] - df[std], df[mean] + df[std], alpha=0.5)
+
+    ax.legend()
+    ax.set_title(title)
+    plt.show()
+    return df, processed_columns
+
+
+def main():
+    df = pd.read_csv('graph_data/fixed_airl.csv')
+    old_runs = [121, 122, 123]
+    new_runs = [162, 163, 166]
+    smoothing_window = 10
+
+    df = clean_df(df)
+
+    run_groups = [old_runs, new_runs]
+    group_names = ['original implementation', 'fixed end reward estimation']
+
+    process_and_plot(df, run_groups, group_names, smoothing_window)
+
+
+if __name__ == '__main__':
+    main()
